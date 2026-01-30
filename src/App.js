522cde3faff875d70156130bf2ec55b0211f3fc5
chrisROct <toastoftsushima@gmail.com>
2026-01-30 16:10:08 +0100

    Refactor BitifyImage component and clean up code

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React, { useState, } from "react";
 import './App.css';
 import { ImageDithering } from "@paper-design/shaders-react";
 
-// Define a functional component named UploadAndDisplayImage
 const BitifyImage = () => {
-  // Define a state variable to store the selected image
   const [selectedImage, setSelectedImage] = useState(null);
 
-
-
-  // Return the JSX for rendering
   return (
     <body className="App-header">
     <div>
-      {/* Header */}
       <h1>Upload and Display Image</h1>
-      {/* Conditionally render the selected image if it exists */}
       {selectedImage && (
-        <div className="App-header"
-        >
-          
+        <div className="App-header">
           {/* Display the selected image and apply the dithering shader provided by @paper-design */}
             <ImageDithering
             width={1280}
@@ -38,31 +29,27 @@ const BitifyImage = () => {
           <button onClick={  function downloadImage() {
             const blob = new Blob([selectedImage], { type: "image/bitmap" });
             const url = URL.createObjectURL(blob);
-
             const link = document.createElement("a");
             link.href = url;
             link.download = "bitified.bmp";
             link.click();
-
             URL.revokeObjectURL(url);
           }}>
           Download bitified image
           </button>
           <br />
-          {/* Button to remove the selected image */}
         </div>
       )}
       <br />
       {/* Input element to select an image file */}
-
       <input
         type="file"
         name="myImage"
         className="App-header"
-        // Event handler to capture file selection and update the state
+        // Event handler to capture file selection and set an image
         onChange={(event) => {
-          console.log(event.target.files[0]); // Log the selected file
-          setSelectedImage(event.target.files[0]); // Update the state with the selected file
+          console.log(event.target.files[0]);
+          setSelectedImage(event.target.files[0]);
         }}
       ></input>
     </div>
@@ -70,5 +57,4 @@ const BitifyImage = () => {
   );
 };
 
-// Export the UploadAndDisplayImage component as default
 export default BitifyImage;
